52850465b731c52da2f9931462378c4c49144132
Da0hn <gabrielhondacba@gmail.com>
2022-11-28 21:00:27 -0400

    feat: add annotations to find base package
    Spring framework equivalence:
    
    - `@Configuration` equivalent to `@Configuration`
    - `@ScanPackage` equivalent to `@ComponentScan`
    - `@ScanPackages` equivalent to `@ComponentScans`

---
+++ core/src/main/java/com/da0hn/coronavac/core/annotations/ScanPackage.java
@@ -0,0 +1,16 @@
+package com.da0hn.coronavac.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Repeatable(ScanPackages.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ScanPackage {
+
+  String value();
+
+}
