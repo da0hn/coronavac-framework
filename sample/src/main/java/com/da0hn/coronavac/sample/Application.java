65ceed95c5344a42c388616b216b15bf0ea0cbe6
Da0hn <gabrielhondacba@gmail.com>
2022-11-28 22:10:42 -0400

    feat: initialize `coronavac` context

--- sample/src/main/java/com/da0hn/coronavac/sample/Application.java
+++ sample/src/main/java/com/da0hn/coronavac/sample/Application.java
@@ -1,9 +1,13 @@
 package com.da0hn.coronavac.sample;
 
-public class Application {
+import com.da0hn.coronavac.core.ApplicationContext;
+
+public final class Application {
 
   public static void main(final String[] args) {
 
+    final ApplicationContext context = new ApplicationContext(ApplicationConfig.class);
+    final var productService = context.find(ProductService.class);
   }
 
 }
