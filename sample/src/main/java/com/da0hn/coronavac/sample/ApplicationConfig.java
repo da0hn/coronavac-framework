a5584df008c52d089bd3e9356578a94c01539e49
Da0hn <gabrielhondacba@gmail.com>
2022-11-28 21:06:28 -0400

    feat: apply configuration annotations

--- sample/src/main/java/com/da0hn/coronavac/sample/ApplicationConfig.java
+++ sample/src/main/java/com/da0hn/coronavac/sample/ApplicationConfig.java
@@ -1,4 +1,9 @@
 package com.da0hn.coronavac.sample;
 
+import com.da0hn.coronavac.core.annotations.Configuration;
+import com.da0hn.coronavac.core.annotations.ScanPackage;
+
+@Configuration
+@ScanPackage(value = "com.da0hn.coronavac.sample")
 public class ApplicationConfig {
 }
