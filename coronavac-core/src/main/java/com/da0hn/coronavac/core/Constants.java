73519beeb1764fad70656114bb19d7d4c288bab8
Da0hn <gabrielhondacba@gmail.com>
2022-11-30 22:53:14 -0400

    feat: add class with common constants

---
+++ coronavac-core/src/main/java/com/da0hn/coronavac/core/Constants.java
@@ -0,0 +1,12 @@
+package com.da0hn.coronavac.core;
+
+public final class Constants {
+
+  public static final String JAVA_CLASS_EXTENSION = ".java";
+  public static final String COMPILED_JAVA_CLASS_EXTENSION = ".class";
+
+  public static final String PACKAGE_SEPARATOR = ".";
+
+  private Constants() {}
+
+}
